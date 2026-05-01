d04e4b4dab5cb6d86af3a09bed5c0261e94ed547
Carolsoush <alvarenga.carolina@hotmail.com>
2026-05-01 00:11:23 +0000

    novas atualizacoes

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,3 +1,31 @@
-const listar = (req,res) => {res.json([])}
+let usuarios = [
+    {
+        "id": 1,
+        "nome": "Carlos Henrique",
+        "telefone": "27999946514",
+        "email": "carlos.henrique@gmail.com",
+        "idade": 28,
+    },
+    {
+        "id": 2,
+        "nome": "Bruno Henrique",
+        "telefone": "28956486452",
+        "email": "bruno.henrique@gmail.com",
+        "idade": 31,
+    },
+]
+const listar = (req, res) => { res.json(usuarios) }
 
-export default {listar}
+
+const buscarPorId = (req, res) => {
+    const id = parseInt(req.params.id)
+    const usuario = usuarios.find(u => u.id === id)
+
+    if (!usuario) {
+        return res.status(404).json({ erro: "Usuário não encontrado" })
+    }
+
+    res.json(usuarios)
+}
+
+export default { listar, buscarPorId }
