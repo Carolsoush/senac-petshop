a8546c61065eed69e7c292ba09c9a90d2957a62a
Carolsoush <alvarenga.carolina@hotmail.com>
2026-05-05 23:56:48 +0000

    criarUsuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,17 +1,19 @@
 let usuarios = [
     {
-        "id": 1,
-        "nome": "Carlos Henrique",
-        "telefone": "27999946514",
-        "email": "carlos.henrique@gmail.com",
-        "idade": 28,
+        id: 1,
+        nome: "Carlos Henrique",
+        telefone: "27999946514",
+        email: "carlos.henrique@gmail.com",
+        idade: 28,
+        senha: "123456"
     },
     {
-        "id": 2,
-        "nome": "Bruno Henrique",
-        "telefone": "28956486452",
-        "email": "bruno.henrique@gmail.com",
-        "idade": 31,
+        id: 2,
+        nome: "Bruno Henrique",
+        telefone: "28956486452",
+        email: "bruno.henrique@gmail.com",
+        idade: 31,
+        senha: "654321"
     },
 ]
 const listar = (req, res) => { res.json(usuarios) }
@@ -27,5 +29,17 @@ const buscarPorId = (req, res) => {
 
     res.json(usuarios)
 }
-
+const criar = (req, res) => {
+    const { nome, email, telefone, idade, senha } = req.body;
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        idade: idade || null,
+        senha: senha
+    };
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+}
 export default { listar, buscarPorId }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,5 +4,7 @@ import uc from '../controllers/usuarios.controllers.js'
 const router = express.Router();
 
 router.get("/", uc.listar);
+router.post("/", uc.criar);
+router.get("/:id", uc.buscarPorId);
 
 export default router;
